0e96f232b1b7eac52928eb146d022b335ea17c1d
hqhs <hqhs@users.noreply.github.com>
2024-08-21 17:37:56 +0400

    add on-save formatting for C buffers

--- lang/cc.el
+++ lang/cc.el
@@ -1,18 +1,19 @@
 ;; -*- lexical-binding: t -*-
 
-(defun +fate/c-mode-common-hook ()
-  (progn (eglot-ensure)
-	 (yas-minor-mode-on)
-	 (tree-sitter-mode)
-	 (tree-sitter-hl-mode)
-	 (outline-minor-mode)))
-
 (defun +fate/add-clang-format-on-save ()
   (add-hook 'before-save-hook
 	    (lambda () (clang-format-buffer))
 	    nil
 	    ;; buffer local hook
 	    t))
+
+(defun +fate/c-mode-common-hook ()
+  (progn (eglot-ensure)
+	 (yas-minor-mode-on)
+	 (tree-sitter-mode)
+	 (tree-sitter-hl-mode)
+	 (outline-minor-mode)
+	 (+fate/add-clang-format-on-save)))
 
 (use-package cc-mode
   :init
@@ -22,7 +23,6 @@
   :config
   (add-hook 'c-mode-hook '+fate/c-mode-common-hook)
   (add-hook 'c++-mode-hook '+fate/c-mode-common-hook)
-  (add-hook 'c++-mode-hook '+fate/add-clang-format-on-save)
   )
 
 (use-package clang-format
