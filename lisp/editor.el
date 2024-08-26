82e7d8cab3f8e588407ce820fffcd6e9cae1e31b
hqhs <hqhs@users.noreply.github.com>
2024-08-26 19:47:03 +0400

    C "once" header snippet & smartparens config

--- lisp/editor.el
+++ lisp/editor.el
@@ -46,6 +46,43 @@
   :config
   (ws-butler-global-mode))
 
+(use-package smartparens
+  :straight t
+  :hook (prog-mode text-mode markdown-mode) ;; add `smartparens-mode` to these hooks
+  :config
+  ;; Load default smartparens rules for various languages
+  (require 'smartparens-config)
+  ;; Overlays are too distracting and not terribly helpful. show-parens does
+  ;; this for us already (and is faster), so...
+  (setq sp-highlight-pair-overlay nil
+        sp-highlight-wrap-overlay nil
+        sp-highlight-wrap-tag-overlay nil)
+  ;; The default is 100, because smartparen's scans are relatively expensive
+  ;; (especially with large pair lists for some modes), we reduce it, as a
+  ;; better compromise between performance and accuracy.
+  (setq sp-max-prefix-length 25)
+  ;; No pair has any business being longer than 4 characters; if they must, set
+  ;; it buffer-locally. It's less work for smartparens.
+  (setq sp-max-pair-length 4)
+  ;; You're likely writing lisp in the minibuffer, therefore, disable these
+  ;; quote pairs, which lisps doesn't use for strings:
+  (sp-local-pair '(minibuffer-mode minibuffer-inactive-mode) "'" nil :actions nil)
+  (sp-local-pair '(minibuffer-mode minibuffer-inactive-mode) "`" nil :actions nil)
+
+  (defvar fate-buffer-smartparens-mode nil)
+  (add-hook 'evil-replace-state-exit-hook
+	     (defun fate-enable-smartparens-mode-maybe-h ()
+	       (when fate-buffer-smartparens-mode
+		 (turn-on-smartparens-mode)
+		 (kill-local-variable 'fate-buffer-smartparens-mode))))
+  (add-hook 'evil-replace-state-entry-hook
+	     (defun fate-disable-smartparens-mode-maybe-h ()
+	       (when smartparens-mode
+		 (setq-local fate-buffer-smartparens-mode t)
+		 (turn-off-smartparens-mode))))
+  )
+
+
 (use-package pcre2el
   :straight t
   :commands (rxt-quote-pcre))
