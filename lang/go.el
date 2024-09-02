4147de2b06e4c7fb9d4f7869a0b09708e33c5170
hqhs <hqhs@users.noreply.github.com>
2024-09-02 18:14:03 +0400

    better go experience

--- lang/go.el
+++ lang/go.el
@@ -1,23 +1,37 @@
 ;; -*- lexical-binding: t -*-
 
+(defvar-local +fate/go-fold-nil-checks t
+  "Buffer-local variable to control folding of nil checks in Go files.")
+
 (defun +fate/hs-hide-go-error-checks ()
   "Hide Go error checking patterns."
   (interactive)
   (save-excursion
     (goto-char (point-min))
-    (while (re-search-forward "\\(if err != nil {\\)" nil t)
+    (while (re-search-forward  "\\(if err\\s-+.*{\\)" nil t)
       (hs-hide-block))))
+
+(defun +fate/toggle-go-nil-checks ()
+  "Toggle folding of nil checks in Go files."
+  (interactive)
+  (setq-local +fate/go-fold-nil-checks (not +fate/go-fold-nil-checks))
+  (if +fate/go-fold-nil-checks
+      (progn
+        (message "Folding nil checks")
+        (+fate/hs-hide-go-error-checks))
+    (progn
+      (message "Unfolding all")
+      (evil-open-folds))))
+
+(defun +fate/go-after-save-hook ()
+  "After-save hook for Go files."
+  (when +fate/go-fold-nil-checks
+    (+fate/hs-hide-go-error-checks)))
 
 (defun +fate/go-mode-hook ()
   ;; Use tabs for indentation
   (setq indent-tabs-mode t)
-  ;; Set tab width to 4 spaces (Go's default)
-  (setq tab-width 2)
-  ;; Ensure that go-mode uses tab-width for indentation
-  (setq-local indent-line-function 'indent-relative)
-  ;; Ensure electric-indent-mode uses tab-width
-  (setq-local electric-indent-inhibit t)
-  ;; If you're using company-mode for completions
+  (setq tab-width 2) ;; since it's tabs, nobody really cares
   (setq-local company-indent-offset 2)
   (progn
     (eglot-ensure)
@@ -25,13 +39,16 @@
     (tree-sitter-hl-mode)
     (yas-minor-mode-on)
     (hs-minor-mode)
+    (add-hook 'after-save-hook '+fate/go-after-save-hook nil t)
     ))
 
 (use-package go-mode
   :straight t
   :init
   :config
+  (add-hook 'go-mode-hook '+fate/go-after-save-hook)
   (add-hook 'go-mode-hook '+fate/go-mode-hook)
+
   (add-hook 'before-save-hook 'gofmt-before-save)
   )
 
