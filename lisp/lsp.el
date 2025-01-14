9c8b4466c4d925534b41ee17d266987a51ba10d4
hqhs <hqhs@users.noreply.github.com>
2025-01-14 20:03:33 +0400

    make use-package eglot config more canonical

--- lisp/lsp.el
+++ lisp/lsp.el
@@ -10,40 +10,39 @@
 
 (use-package eglot
   :commands (eglot eglot-ensure)
-  ;; eglot comes with emacs starting from 30.0
-  ;; :straight t
   :init
   (setq eglot-autoshutdown t
-
-	;; to view capabilities:
-	;; (kill-new (format "%S" (eglot--capabilities (eglot--current-server-or-lose))))
-	eglot-ignored-server-capabilities '(:documentOnTypeFormattingProvider)
-        ;; NOTE This setting disable the eglot-events-buffer enabling more
-        ;;      consistent performance on long running emacs instance.
-        ;;      Default is 2000000 lines. After each new event the whole buffer
-        ;;      is pretty printed which causes steady performance decrease over time.
-        ;;      CPU is spent on pretty priting and Emacs GC is put under high pressure
-        eglot-events-buffer-size 0)
+        eglot-events-buffer-size 0
+        eglot-ignored-server-capabilities '(:documentOnTypeFormattingProvider))
 
   :config
-  ;; replace
+  ;; Define server programs with all options preserved
   (setq eglot-server-programs
-	(cons '(c++-mode . ("clangd" "--header-insertion=never"))
-	      (cl-remove-if (lambda (entry)
-			      (and (listp (car entry))
-				   (member 'c++-mode (car entry))))
-			    eglot-server-programs)))
-  ;; add
-  (push '(c-mode . ("clangd" "--header-insertion=never"))
-	eglot-server-programs)
+        `(;; C/C++ with preserved options
+          (c++-mode . ("clangd" "--header-insertion=never"))
+          (c-mode . ("clangd" "--header-insertion=never"))
+          ;; Preserve any other language servers from eglot defaults
+          ,@(cl-remove-if (lambda (entry)
+                           (and (listp (car entry))
+                                (or (member 'c++-mode (car entry))
+                                    (eq (car entry) 'c-mode))))
+                         eglot-server-programs)))
 
-  (add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1)))
-  (add-hook 'eglot-managed-mode-hook
-   (lambda ()
-     ;; we want eglot to setup callbacks from eldoc, but we don't want eldoc
-     ;; running after every command. As a workaround, we disable it after we just
-     ;; enabled it. Now calling `M-x eldoc` will put the help we want in the eldoc
-     ;; buffer. Alternatively we could tell eglot to stay out of eldoc, and add
-     ;; the hooks manually, but that seems fragile to updates in eglot.
-     (eldoc-mode -1)))
-  (add-to-list 'eglot-stay-out-of 'flymake))
+  ;; Hooks setup
+  :hook
+  ((eglot-managed-mode
+    . (lambda ()
+        ;; Disable inlay hints
+        (eglot-inlay-hints-mode -1)
+        ;; Disable automatic eldoc but keep eglot's eldoc setup
+        ;; This preserves the ability to manually trigger docs with eldoc
+        (eldoc-mode -1)))
+   ;; Stay out of flymake
+   (after-init . (lambda ()
+                   (add-to-list 'eglot-stay-out-of 'flymake))))
+
+  ;; Optional: Add any additional customization
+  :custom
+  (eglot-connect-timeout 30)
+  (eglot-sync-connect nil)
+  (eglot-autoreconnect t))
