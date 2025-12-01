92a00e6acaa4c3b4cdb7b886f2b195bc111b3bbf
hqhs <hqhs@users.noreply.github.com>
2025-12-01 14:05:41 +0400

    remove consult from autoloads helpers

--- lisp/autoloads.el
+++ lisp/autoloads.el
@@ -38,88 +38,45 @@
     (region-active-p)))
 
 ;;;###autoload
-(cl-defun +vertico-file-search (&key query in all-files (recursive t) prompt args)
-  "Conduct a file search using ripgrep.
-
-:query STRING
-  Determines the initial input to search for.
-:in PATH
-  Sets what directory to base the search out of. Defaults to the current project's root.
-:recursive BOOL
-  Whether or not to search files recursively from the base directory.
-:args LIST
-  Arguments to be appended to `consult-ripgrep-args'."
-  (declare (indent defun))
-  (unless (executable-find "rg")
-    (user-error "Couldn't find ripgrep in your PATH"))
-  (require 'consult)
-  (setq deactivate-mark t)
-  (let* ((project-root (project-root (project-current t)))
-         (directory (or in project-root))
-         (consult-ripgrep-args
-          (concat "rg "
-                  (if all-files "-uu ")
-                  (unless recursive "--maxdepth 1 ")
-                  "--null --line-buffered --color=never --max-columns=1000 "
-                  "--path-separator /   --smart-case --no-heading "
-                  "--with-filename --line-number --search-zip "
-                  "--hidden -g !.git -g !.svn -g !.hg "
-                  (mapconcat #'identity args " ")))
-         (prompt (if (stringp prompt) (string-trim prompt) "Search"))
-         (query (or query
-                   (when (+fate/region-active-p)
-                     (regexp-quote (buffer-substring-no-properties (region-beginning) (region-end))))))
-	 (consult-async-split-style consult-async-split-style)
-         (consult-async-split-styles-alist consult-async-split-styles-alist))
-    ;; Change the split style if the initial query contains the separator.
-    (when query
-      (cl-destructuring-bind (&key type separator initial _function)
-          (consult--async-split-style)
-        (pcase type
-          (`separator
-           (replace-regexp-in-string (regexp-quote (char-to-string separator))
-                                   (concat "\\" (char-to-string separator))
-                                   query t t))
-          (`perl
-           (when (string-match-p initial query)
-             (setf (alist-get 'perlalt consult-async-split-styles-alist)
-                   `(:initial ,(or (cl-loop for char in (list "%" "@" "!" "&" "/" ";")
-                                          unless (string-match-p char query)
-                                          return char)
-                                 "%")
-                     :type perl)
-                   consult-async-split-style 'perlalt))))))
-    (consult--grep prompt #'consult--ripgrep-make-builder directory query)))
-
-;;;###autoload
-(defun +vertico/project-search (&optional arg initial-query directory)
-  "Performs a live project search from the project root using ripgrep.
-If ARG (universal argument), include all files, even hidden or compressed ones."
+(defun +fate/project-search (&optional arg)
+  "Search project root with ripgrep. With ARG, include hidden files."
   (interactive "P")
-  (+vertico-file-search :query initial-query :in directory :all-files arg))
-
-;;;###autoload
-(defun +vertico/project-search-from-cwd (&optional arg initial-query)
-  "Performs a live project search from the current directory."
-  (interactive "P")
-  (+vertico/project-search arg initial-query default-directory))
+  (+fate--rg-search (project-root (project-current t)) arg))
 
 ;;;###autoload
 (defun +fate/search-cwd (&optional arg)
-  "Conduct a text search in files under the current folder.
-If prefix ARG is set, prompt for a directory to search from."
+  "Search current directory with ripgrep. With ARG, include hidden files."
   (interactive "P")
-  (let ((default-directory
-          (if arg
-              (read-directory-name "Search directory: ")
-            default-directory)))
-    (call-interactively #'+vertico/project-search-from-cwd)))
+  (+fate--rg-search default-directory arg))
 
 ;;;###autoload
-(defun +fate/search-other-cwd ()
-  "Conduct a text search in another directory."
-  (interactive)
-  (+fate/search-cwd 'other))
+(defun +fate/search-other-cwd (&optional arg)
+  "Search from a prompted directory."
+  (interactive "P")
+  (+fate--rg-search (read-directory-name "Search in: ") arg))
+
+(defun +fate--rg-search (directory &optional include-hidden)
+  "Run ripgrep in DIRECTORY. With INCLUDE-HIDDEN, search hidden files."
+  (unless (executable-find "rg")
+    (user-error "Couldn't find ripgrep in your PATH"))
+  (let* ((query (or (when (use-region-p)
+                      (buffer-substring-no-properties (region-beginning) (region-end)))
+                    (read-string "Search: ")))
+         (default-directory directory)
+         (cmd (format "rg --no-heading --line-number --column --smart-case %s %s"
+                      (if include-hidden "--hidden --no-ignore" "")
+                      (shell-quote-argument query)))
+         (hits (split-string (shell-command-to-string cmd) "\n" t)))
+    (xref-show-xrefs
+     (mapcar (lambda (hit)
+               (when (string-match "\\`\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\):\\(.*\\)" hit)
+                 (xref-make (match-string 4 hit)
+                            (xref-make-file-location
+                             (expand-file-name (match-string 1 hit) directory)
+                             (string-to-number (match-string 2 hit))
+                             (string-to-number (match-string 3 hit))))))
+             hits)
+     nil)))
 
 ;;;###autoload
 (defun +fate/search-project (&optional arg)
@@ -134,8 +91,8 @@ If prefix ARG is set, include hidden files."
                  (user-error "There are no known projects")))
            default-directory)))
     (if (eq arg 'other)
-        (call-interactively #'+vertico/project-search)
-      (+vertico/project-search arg))))
+        (call-interactively #'+fate/project-search)
+      (+fate/project-search arg))))
 
 ;;;###autoload
 (defun +fate/search-other-project ()
