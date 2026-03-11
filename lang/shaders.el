f940cb47d8fb0fb56f13de2504fdfa0130ab60bd
Dima Afanasyev <dimaafanasev@example.com>
2026-03-11 19:04:25 +0400

    vendor glsl mode

--- lang/shaders.el
+++ lang/shaders.el
@@ -4,5 +4,11 @@
 (when (locate-library "wgsl-mode")
   (use-package wgsl-mode))
 
-(when (locate-library "glsl-mode")
-  (use-package glsl-mode))
+(use-package glsl-mode
+  :mode (("\\.glsl\\'" . glsl-mode)
+         ("\\.vert\\'" . glsl-mode)
+         ("\\.frag\\'" . glsl-mode)
+         ("\\.geom\\'" . glsl-mode)
+         ("\\.comp\\'" . glsl-mode)))
+
+(provide 'shaders)
